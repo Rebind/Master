540c67a963b8cc552e973c49e3fb2befc7f79f51
AGirard <agirard@ucsc.edu>
2016-01-28 19:31:59 -0800

    updated to master3.0
    updated prefabs

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private Controller2D player;
+
+    public GameObject deathParticle;
+   
+
+    public float respawnDelay;
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Controller2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void respawnPlayer()
+    {
+        StartCoroutine("respawnPlayerCo");
+
+    }
+
+    public IEnumerator respawnPlayerCo()
+    {
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        yield return new WaitForSeconds(respawnDelay);
+		SceneManager.LoadScene ("ninja");
+      //  Application.LoadLevel(Application.loadedLevel);
+        yield return new WaitForSeconds(respawnDelay);
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        
+        //Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+        //player.transform.position = currentCheckpoint.transform.position;
+    }
+}
---
+++ Assets/Scripts/PushBox.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushBox : MonoBehaviour
+{
+    private GameObject Player;
+    private MergeAttachDetach arm;
+    private Rigidbody2D rgbd;
+    private Animator myAnimator;
+	private Vector3 addGap = new Vector3(.3f,0,0);
+	private Vector3 temPosition;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        arm = Player.GetComponent<MergeAttachDetach>();
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Debug.Log(gameObject.transform.position.x);
+		pushController();
+        
+    }
+
+	private void pushController(){
+		if (Input.GetKeyDown(KeyCode.H) && (arm.hasArm || arm.hasSecondArm))
+		{
+			//get the position when player press "h"
+			temPosition = gameObject.transform.position;
+			rgbd.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+			gameObject.layer = 11;
+		}
+		if (Input.GetKeyUp(KeyCode.H) && (arm.hasArm || arm.hasSecondArm))
+		{
+			rgbd.constraints = RigidbodyConstraints2D.FreezeAll;
+			gameObject.layer = 8;
+			//make a gap 
+			if (gameObject.transform.position.x > temPosition.x) {
+				gameObject.transform.position += addGap;
+			} else if (gameObject.transform.position.x < temPosition.x) {
+				gameObject.transform.position -= addGap;
+			}
+		}
+	}
+
+	//no press key requise
+	private void pushController2(){
+		if (arm.hasArm || arm.hasSecondArm)
+		{
+			rgbd.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+			gameObject.layer = 11;
+		}
+		if (arm.hasArm == false && arm.hasSecondArm == false)
+		{
+			rgbd.constraints = RigidbodyConstraints2D.FreezeAll;
+			gameObject.layer = 8;
+		}
+	}
+
+
+
+}
+
