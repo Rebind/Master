d4c57be2581b628f9a6ffab6ea91ec426e910a58
JoeCaveman <jjosueordonez@gmail.com>
2016-02-02 04:04:15 -0800

    animation stuff and collison boxes

---
+++ Assets/Scripts/Limbs.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Limbs : MonoBehaviour {
+
+    private Rigidbody2D myRigidLimb;
+    private Animator myAnim;
+    private CameraFollow camScript;
+    private Controller2D limb;
+    private GameObject myGO;
+
+
+    // Use this for initialization
+    void Start () {
+        myGO = GameObject.FindGameObjectWithTag("MainCamera");
+        camScript = myGO.GetComponent<CameraFollow>();
+        myRigidLimb = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+
+     
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        limb = camScript.target;
+ 
+        if (limb.name == gameObject.name)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            myAnim.SetFloat("spped", Mathf.Abs(horizontal));
+        }
+        else
+        {
+            myAnim.SetFloat("spped", 0);
+        }
+
+    }
+}
