9679d52044e0b962f26c33655ec19532d09afd62
xxxPeace <rtan9@ucsc.edu>
2016-02-12 04:47:45 -0800

    fix nose

--- Assets/Scripts/Nose.cs
+++ Assets/Scripts/Nose.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class Nose : MonoBehaviour {
 
+	public Player playerScript;
 	private GameObject Player;
 	//private Vector3 addHigh = new Vector3(0,20.0f,0);
-	private float addHigh = 20f;
-	private float timer = 0;
-	private float timerMax = 8;
-	public Player playerScript;
+	private float addHigh;
+	private float timer;
+	private float timerMax;//time for adding high to the player
 
 	// Use this for initialization
 	public void Start () {
 		Player = GameObject.Find("Player");
 		playerScript = Player.GetComponent<Player>();
-
+		addHigh = 20f;
+		timer = 0;
+		timerMax = 8;//time for adding high to the player
 	}
 
 	void Update(){
@@ -24,6 +26,7 @@ public class Nose : MonoBehaviour {
 			addHigh = 20f;
 			// reset timer
 		} 
+		//stop adding high
 		if (timer > timerMax+5) {
 			addHigh = 0;
 			timer = 0;
@@ -33,9 +36,8 @@ public class Nose : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D other){
 
 		if (other.CompareTag ("Player") || other.CompareTag ("arm") || other.CompareTag ("torso") || other.CompareTag ("leg")) {
-			Debug.Log (playerScript.velocity.y);
+			//Debug.Log (playerScript.velocity.y);
 			playerScript.notOnNose = false;
-			//other.GetComponent<Player> ().velocity += addHigh;
 			playerScript.velocity.y += addHigh * Time.deltaTime;
 		}
 	}
