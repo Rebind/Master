5d2f1b99ce4ad39b266eb19fd9c6b35e33678f6b
JoeCaveman <jjosueordonez@gmail.com>
2016-02-25 01:26:34 -0800

    mouth door

--- Assets/Scripts/PPlates,Levers,Doors/DoorController.cs
+++ Assets/Scripts/PPlates,Levers,Doors/DoorController.cs
@@ -1,77 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DoorController : MonoBehaviour {
+public class DoorController : MonoBehaviour
+{
 
-	// Use this for initialization
+    // Use this for initialization
 
-	public bool open;
+    public bool open;
 
-	public bool requireTwoPlates;
-    public Animator mouth;
-	private bool startState;
-	private BoxCollider2D myCollider;
+    public bool requireMultiplePlates;
+    private bool startState;
+    private BoxCollider2D myCollider;
     private Rigidbody2D myRigidBody;
+    public Animator mouthDoorAnimator;
 
 
-	public bool plateOne;
-	public bool plateTwo;
+    public int neededToOpen;
 
-	void Start () {
-		assignState ();
-        mouth = GetComponent<Animator>();
-		myCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
-        myRigidBody = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+    [HideInInspector]
+    public int platesActivated;
+
+    void Start()
+    {
+        assignState();
+        myCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
+        mouthDoorAnimator = GetComponent<Animator>();
         startState = open;
 
     }
 
 
 
-    void assignState(){
-		myCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
+    void assignState()
+    {
+        myCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
 
-		if (open) {
-			Debug.Log ("open");
-            mouth.SetBool("open", true);		//gameObject.GetComponent<MeshRenderer> ().enabled = false;
-			myCollider.size = new Vector2(0,0);
-           // myRigidBody.gravityScale = 1;
+        if (open)
+        {
+            Debug.Log("open");
+            
+            //gameObject.GetComponent<MeshRenderer>().enabled = false;
+            myCollider.size = new Vector2(0, 0);
+            mouthDoorAnimator.SetBool("open", true);
 
 
-		} else if (!open) {
-			Debug.Log ("closed");
-            //gameObject.GetComponent<MeshRenderer> ().enabled = true;
-            mouth.SetBool("open", false);
-            if (gameObject.name == "mouth_door")
-            {
-                myCollider.size = new Vector2(1.5f, 0.8f);
-            }
-            else
-            {
-                myCollider.size = new Vector2(1, 1);
-            }
+        }
+        else if (!open)
+        {
+            Debug.Log("closed");
+            //gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+            //if (gameObject.name == "mouth_door")
+           // {
+               
+            myCollider.size = new Vector2(1.5f, 0.8f);
+            mouthDoorAnimator.SetBool("open", false);
+            // }
+
+            //else {
+            //myCollider.size = new Vector2(1, 1);
+            //}
         }
     }
 
 
-	public void toggle(){
-		open = !open;
-		assignState ();
+    public void toggle()
+    {
+        open = !open;
+        assignState();
 
-	}
+    }
 
 
     public void turnOn()
     {
-		if (!requireTwoPlates) {
-			open = !startState;
-			assignState ();
-		} else if (requireTwoPlates) {
-			if (plateOne && plateTwo) {
-				open = !startState;
-				assignState ();
-			}
-		}
+        if (!requireMultiplePlates)
+        {
+            open = !startState;
+            assignState();
+        }
+        else if (requireMultiplePlates)
+        {
+            Debug.Log(platesActivated);
+            if (platesActivated == neededToOpen)
+            {
+                open = !startState;
+                assignState();
+            }
+        }
     }
 
     public void turnOff()
