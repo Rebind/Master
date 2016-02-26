b2d41c1805ed451d287e20ef5632a87e3fbfbb64
JoeCaveman <jjosueordonez@gmail.com>
2016-02-25 22:18:13 -0800

    fixed door script

--- Assets/Scripts/PPlates,Levers,Doors/DoorController.cs
+++ Assets/Scripts/PPlates,Levers,Doors/DoorController.cs
@@ -7,26 +7,19 @@ public class DoorController : MonoBehaviour {
 
 	public bool open;
 
-<<<<<<< HEAD
 	public bool requireMultiplePlates;
-=======
 	public bool requireTwoPlates;
     public Animator mouth;
->>>>>>> refs/remotes/origin/Josue_MyMaster
 	private bool startState;
 	private BoxCollider2D myCollider;
     private Rigidbody2D myRigidBody;
 
-
-<<<<<<< HEAD
 	public int neededToOpen;
 
 	[HideInInspector]
 	public int platesActivated;
-=======
 	public bool plateOne;
 	public bool plateTwo;
->>>>>>> refs/remotes/origin/Josue_MyMaster
 
 	void Start () {
 		assignState ();
@@ -43,26 +36,11 @@ public class DoorController : MonoBehaviour {
 
 		if (open) {
 			Debug.Log ("open");
-<<<<<<< HEAD
-
-			gameObject.GetComponent<MeshRenderer> ().enabled = false;
-			myCollider.size = new Vector2(0,0);
-=======
             mouth.SetBool("open", true);		//gameObject.GetComponent<MeshRenderer> ().enabled = false;
 			myCollider.size = new Vector2(0,0);
            // myRigidBody.gravityScale = 1;
->>>>>>> refs/remotes/origin/Josue_MyMaster
-
-
 		} else if (!open) {
 			Debug.Log ("closed");
-<<<<<<< HEAD
-			gameObject.GetComponent<MeshRenderer> ().enabled = true;
-
-			myCollider.size = new Vector2(1,1);
-		}
-	}
-=======
             //gameObject.GetComponent<MeshRenderer> ().enabled = true;
             mouth.SetBool("open", false);
             if (gameObject.name == "mouth_door")
@@ -75,8 +53,6 @@ public class DoorController : MonoBehaviour {
             }
         }
     }
->>>>>>> refs/remotes/origin/Josue_MyMaster
-
 
 	public void toggle(){
 		open = !open;
--- Assets/Scripts/PPlates,Levers,Doors/PressurePlateController.cs
+++ Assets/Scripts/PPlates,Levers,Doors/PressurePlateController.cs
@@ -1,120 +1,140 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PressurePlateController : MonoBehaviour {
+public class PressurePlateController : MonoBehaviour
+{
 
-	BoxCollider2D myCollider;
+    BoxCollider2D myCollider;
 
 
-	public bool moveDoor;
-	public bool movePlatform;
+    public bool moveDoor;
+    public bool movePlatform;
     public Sprite activeSprite;
     public Sprite inactiveSprite;
 
-	public GameObject[] affectedDoors;
-	public GameObject[] affectedPlatforms;
-	SpriteRenderer spriteRenderer;
+    public GameObject[] affectedDoors;
+    public GameObject[] affectedPlatforms;
+    SpriteRenderer spriteRenderer;
 
 
 
-	bool onPlate;
-	bool oneTime;
+    bool onPlate;
+    bool oneTime;
 
 
 
-	// Use this for initialization
-	public void Start () {
-		oneTime = false;
-		onPlate = false;
-		spriteRenderer = this.gameObject.GetComponent<SpriteRenderer> ();
+    // Use this for initialization
+    public void Start()
+    {
+        oneTime = false;
+        onPlate = false;
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
 
-	}
+    }
 
 
 
-	void OnTriggerStay2D(Collider2D other)
-	{
-		if (!oneTime) {
-			if (other.CompareTag ("Player") || other.CompareTag ("arm") || other.CompareTag ("torso") || other.CompareTag ("leg")) {
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (!oneTime)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("arm") || other.CompareTag("torso") || other.CompareTag("leg"))
+            {
 
-				spriteRenderer.color = new Color(0f, 1f, 0f, 1f);
+                spriteRenderer.color = new Color(0f, 1f, 0f, 1f);
                 //spriteRenderer.sprite = activeSprite;
-				onPlate = true;
+                onPlate = true;
 
-				if (moveDoor) { //toggles the door(s) states
+                if (moveDoor)
+                { //toggles the door(s) states
 
-						foreach (GameObject door in affectedDoors) {
+                    foreach (GameObject door in affectedDoors)
+                    {
 
-						if (!door.GetComponent<DoorController> ().requireMultiplePlates) {
-								door.GetComponent<DoorController> ().turnOn ();
-						} else if (door.GetComponent<DoorController> ().requireMultiplePlates) {
-							door.GetComponent<DoorController> ().platesActivated++;
-							door.GetComponent<DoorController> ().turnOn ();
+                        if (!door.GetComponent<DoorController>().requireMultiplePlates)
+                        {
+                            door.GetComponent<DoorController>().turnOn();
+                        }
+                        else if (door.GetComponent<DoorController>().requireMultiplePlates)
+                        {
+                            door.GetComponent<DoorController>().platesActivated++;
+                            door.GetComponent<DoorController>().turnOn();
 
 
-							}
-						}
+                        }
+                    }
 
 
-					
 
-				}
-				if (movePlatform) { //toggles the state of the platform(s)
-					foreach (GameObject platform in affectedPlatforms) {
 
-						platform.GetComponent<PlatformController> ().turnOn ();
-					}
-				}
-			}
-			oneTime = true;
-		}
-	}
+                }
+                if (movePlatform)
+                { //toggles the state of the platform(s)
+                    foreach (GameObject platform in affectedPlatforms)
+                    {
 
-	void OnTriggerExit2D(Collider2D other)
-	{
-		if (other.CompareTag("Player")|| other.CompareTag("arm") || other.CompareTag("torso") || other.CompareTag("leg"))
-		{
-			spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
+                        platform.GetComponent<PlatformController>().turnOn();
+                    }
+                }
+            }
+            oneTime = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("arm") || other.CompareTag("torso") || other.CompareTag("leg"))
+        {
+            spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
             //spriteRenderer.sprite = inactiveSprite;
 
             onPlate = false;
-			Debug.Log ("Off Plate");
-			//Destroy (this.gameObject);
+            Debug.Log("Off Plate");
+            //Destroy (this.gameObject);
 
-			if (moveDoor) { //toggles the door(s) states
+            if (moveDoor)
+            { //toggles the door(s) states
 
-				foreach (GameObject door in affectedDoors) {
+                foreach (GameObject door in affectedDoors)
+                {
 
-					if (!door.GetComponent<DoorController> ().requireMultiplePlates) {
-						door.GetComponent<DoorController> ().turnOff ();
-					} else if (door.GetComponent<DoorController> ().requireMultiplePlates) {
-						door.GetComponent<DoorController> ().platesActivated--;
-						door.GetComponent<DoorController> ().turnOff ();
+                    if (!door.GetComponent<DoorController>().requireMultiplePlates)
+                    {
+                        door.GetComponent<DoorController>().turnOff();
+                    }
+                    else if (door.GetComponent<DoorController>().requireMultiplePlates)
+                    {
+                        door.GetComponent<DoorController>().platesActivated--;
+                        door.GetComponent<DoorController>().turnOff();
 
-					}
-				}
-
-
-
-
-			} if (movePlatform) { //toggles the state of the platform(s)
-				foreach (GameObject platform in affectedPlatforms) {
-
-					platform.GetComponent<PlatformController> ().turnOff ();
-				}
-
-			}
-		} oneTime = false;
-	}
-
-
-	// Update is called once per frame
+                    }
+                }
 
 
 
 
+            }
+            if (movePlatform)
+            { //toggles the state of the platform(s)
+                foreach (GameObject platform in affectedPlatforms)
+                {
+
+                    platform.GetComponent<PlatformController>().turnOff();
+                }
+
+            }
+        }
+        oneTime = false;
+    }
 
 
-	//returns true or false if plate is colliding with an object
+    // Update is called once per frame
+
+
+
+
+
+
+    //returns true or false if plate is colliding with an object
 
 }
