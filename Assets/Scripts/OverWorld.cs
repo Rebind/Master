4f5792355ea74a88a9bcb41205197da16ff40b7d
Khoa Luong <tangentminds@yahoo.com>
2016-03-09 00:48:49 -0800

    Added Overworld Scene
    .

---
+++ Assets/Scripts/OverWorld.cs
@@ -0,0 +1,192 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class OverWorld : MonoBehaviour {
+    //private GameObject player;
+    public LinkedList Levels;
+    private GameObject tmpLevel;
+
+    public float speed = 50f;
+
+	public class Node {
+        public Node next;
+        public string left;
+        public string right;
+        public string up;
+        public string down;
+        public string level;
+        public Vector3 position;
+        public bool finished;
+    }
+    //LinkedList data structure to hold gameobjects and position
+    public class LinkedList
+    {
+        private Node head;
+        private Node last;
+        private Node curr;
+        //start a new node head
+        public void addHead(string levelname, Vector3 position) {
+            Node toAdd      = new Node();
+            toAdd.level     = levelname;
+            toAdd.position  = position;
+            toAdd.finished  = false;
+            toAdd.next      = null;
+            toAdd.left      = null;
+            toAdd.right     = null;
+            toAdd.up        = null;
+            toAdd.down      = null;
+            curr        = toAdd;
+            head        = toAdd;
+            last        = toAdd;
+        }
+        //attach a new gameobject to the last element
+        public void addNode(string levelname, string parent, string relativePosition, 
+            Vector3 position) {
+            Node toAdd      = new Node();
+            toAdd.level     = levelname;
+            toAdd.position  = position;
+            toAdd.finished  = false;
+            toAdd.next 	    = null;
+           	toAdd.left 	    = null;
+            toAdd.right     = null;
+            toAdd.up 	    = null;
+            toAdd.down 	    = null;
+
+            if(head != null) {
+            	last.next	= toAdd;
+            	last 		= toAdd;
+
+            	Node iter = head;
+            	while(iter != null){
+            		if(iter.level == parent) {
+            			break;
+            		} else {
+            			iter = iter.next;
+            		}
+            	}
+            	switch(relativePosition){
+            		case "up":
+            			iter.up 	= toAdd.level;
+            			toAdd.down 	= iter.level;
+            			break;
+            		case "down":
+            			iter.down 	= toAdd.level;
+            			toAdd.up 	= iter.level;
+            			break;
+            		case "left":
+            			iter.left 	= toAdd.level;
+            			toAdd.right = iter.level;
+            			break;
+            		case "right":
+            			iter.right 	= toAdd.level;
+            			toAdd.left 	= iter.level;
+            			break;
+            		default:
+            			break;
+            	}
+            }
+        }
+        //print all elements of list
+        public void printall() {
+        	Node iter = head;
+        	while(iter != null){
+        		Debug.Log(iter.level);
+        		Debug.Log(iter.position);
+        		Debug.Log(iter.left);
+        		Debug.Log(iter.right);
+        		Debug.Log(iter.up);
+        		Debug.Log(iter.down);
+        		iter = iter.next;
+        	}
+        }
+        //find level
+        public string findLevel(string level) {
+        	string tmp = curr.level;
+        	Node iter = head;
+        	while(iter != null) {
+        		if(iter.level == level) {
+        			curr = iter;
+        			tmp = iter.level;
+        			return tmp;
+        		} else {
+        			iter = iter.next;
+        		}
+        	}
+        	return tmp;
+        }
+        //move current node and get its position
+        public string getNewPos(string direction) {
+            string position = curr.level;
+        	switch(direction) {
+        		case "up":
+        			if(curr.up != null) {
+        				position = findLevel(curr.up);
+        				return position;
+        			} else {
+        				return position;
+        			}
+        		case "down":
+        			if(curr.down != null) {
+                        position = findLevel(curr.down);
+        				return position;
+        			} else {
+        				return position;
+        			}
+        		case "left":
+        			if(curr.left != null) {
+                        position = findLevel(curr.left);
+        				return position;
+        			} else {
+        				return position;
+        			}
+        		case "right":
+        			if(curr.right != null) {
+                        position = findLevel(curr.right);
+        				return position;
+        			} else {
+        				return position;
+        			}
+                case "none":
+                    return position;
+        	}
+            return position;
+        }
+        //get the current level of the current node
+        public string getCurr() {
+            string tmp = null;
+            if (curr != null){
+                tmp = curr.level;
+                return tmp;
+            } else {
+                return tmp;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Levels = new LinkedList();
+        //Put the levels here
+        tmpLevel = GameObject.Find("Blah");
+        Levels.addHead(tmpLevel.name, tmpLevel.transform.position);
+
+        tmpLevel = GameObject.Find("Blah1");
+        Levels.addNode(tmpLevel.name, "Blah", "left", tmpLevel.transform.position);
+
+        tmpLevel = GameObject.Find("BlahBlah");
+        Levels.addNode(tmpLevel.name, "Blah", "right", tmpLevel.transform.position);
+
+        tmpLevel = GameObject.Find("BlahBlahBlah");
+        Levels.addNode(tmpLevel.name, "Blah", "up", tmpLevel.transform.position);
+
+        tmpLevel = GameObject.Find("BlahBlahBlahBlah");
+        Levels.addNode(tmpLevel.name, "Blah", "down", tmpLevel.transform.position);
+
+        Levels.printall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/PlayerOW.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOW : MonoBehaviour {
+	private OverWorld OWScript;
+	private GameObject OW;
+	private GameObject player;
+	private GameObject target;
+	public Vector3 targetPos;
+
+	private void setNewTarget() {
+		string tmp = null;
+		if (Input.GetKeyUp(KeyCode.W)) {
+            tmp = OWScript.Levels.getNewPos("up");
+            target = GameObject.Find(tmp);
+        }
+        if (Input.GetKeyUp(KeyCode.A)) {
+            tmp = OWScript.Levels.getNewPos("left");
+            target = GameObject.Find(tmp);
+        }
+        if (Input.GetKeyUp(KeyCode.S)) {
+            tmp = OWScript.Levels.getNewPos("down");
+            target = GameObject.Find(tmp);
+        }
+        if (Input.GetKeyUp(KeyCode.D)) {
+            tmp = OWScript.Levels.getNewPos("right");
+            target = GameObject.Find(tmp);
+        }
+        if (Input.GetKeyUp(KeyCode.Return)) {
+            tmp = OWScript.Levels.getCurr();
+            target = GameObject.Find(tmp);
+            if (target.transform.position == player.transform.position){
+            	//load level using tmp as level name;
+            }
+        }
+	}
+
+	// Use this for initialization
+	void Start () {
+		OW = GameObject.Find("OverWorld");
+		OWScript = OW.GetComponent<OverWorld>();
+		player = GameObject.Find("PlayerOW");
+		target = GameObject.Find("Blah");
+		player.transform.position = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setNewTarget();
+		targetPos = new Vector3(target.transform.position.x, target.transform.position.y, 
+			target.transform.position.z);
+		transform.position = Vector3.MoveTowards(transform.position, targetPos, 0.1f);
+	}
+}
