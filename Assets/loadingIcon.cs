4999e1520abfcc2ad5825db44543a8b763cb6b10
JoeCaveman <jjosueordonez@gmail.com>
2016-04-14 03:07:31 -0700

    loading screen polish and level polish

---
+++ Assets/loadingIcon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadingIcon : MonoBehaviour {
+
+    private int num;
+
+	// Use this for initialization
+	void Start () {
+        num = (int)Mathf.Floor(Random.Range(0, 7));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.GetComponent<Animator>().SetInteger("ranNum", num);
+	
+	}
+}
