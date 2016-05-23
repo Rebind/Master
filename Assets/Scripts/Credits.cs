60e3234ca59bea6e14ed5e9fff68c38860cbaa85
danicapok <dpok@ucsc.edu>
2016-05-23 10:32:23 -0700

    Added Credits Scene
    Still in progress

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Credits : MonoBehaviour
+{
+	private float offset;
+	public float speed = 29.0f;
+	public GUIStyle style;
+	public Rect viewArea;
+
+	private void Start()
+	{
+		this.offset = this.viewArea.height;
+	}
+
+	private void Update()
+	{
+		this.offset -= Time.deltaTime * this.speed;
+	}
+
+	private void OnGUI()
+	{
+		GUI.BeginGroup(this.viewArea);
+
+		var position = new Rect(0, this.offset, this.viewArea.width, this.viewArea.height);
+		var text = @"Lead Designer/Programmer: Josue Ordonez
+		Lead Tester: Edgar Lopez
+		Artist Coordinator/Programmer: Khoa Luong
+		Lead Producer/Programmer: Alex Girard
+		User Test Coordinator/Programmer: Rihui Tan
+		Lead Developer/Programmer: Danica Pok
+		Artist: Mindy Nguyen and Juan Morales
+		Sound Designer: Joshua Ray";
+
+		GUI.Label(position, text, this.style);
+
+		GUI.EndGroup();
+	}
+}
