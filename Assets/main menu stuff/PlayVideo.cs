6c64ea4f6118e9e051b060c1db2bb33b55581001
JoeCaveman <jjosueordonez@gmail.com>
2016-05-26 02:23:37 -0700

    made main menu screen

---
+++ Assets/main menu stuff/PlayVideo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+    private AudioSource myaudio;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        myaudio = GetComponent<AudioSource>();
+        myaudio.clip = movie.audioClip;
+        movie.Play();
+        myaudio.Play();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
