922ce4275b30f87b603a1c4ac5414d5f0866383e
danicapok <dpok@ucsc.edu>
2016-05-31 19:38:55 -0700

    Added old concept images to credits scene

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -11,11 +11,14 @@ public class Credits : MonoBehaviour
 	public string level;
 	public GameObject text;
 	public GameObject title;
-
+	public GameObject charColors;
+	public GameObject sampleTorsoMove;
 
 	private void Start(){
 		text = GameObject.Find ("Text (1)");
 		title = GameObject.Find ("REBINDTitle");
+		charColors = GameObject.Find ("CharColors");
+		sampleTorsoMove = GameObject.Find ("SampleTorsoMove");
 	}
 
 	private void Update()
@@ -23,6 +26,8 @@ public class Credits : MonoBehaviour
 		//camera.transform.Translate (Vector3.down * Time.deltaTime * speed);
 		text.transform.Translate (Vector3.up * Time.deltaTime * speed);
 		title.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		charColors.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		sampleTorsoMove.transform.Translate (Vector3.up * Time.deltaTime * speed);
 		StartCoroutine ("waitFor");
 	}
 
