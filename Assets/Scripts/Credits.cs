ec41ae2c1f58389dd53cd37c01c4f879b6e15f09
danicapok <dpok@ucsc.edu>
2016-06-01 11:45:38 -0700

    added more stuff for the credits scene

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -13,12 +13,19 @@ public class Credits : MonoBehaviour
 	public GameObject title;
 	public GameObject charColors;
 	public GameObject sampleTorsoMove;
+	public GameObject rebindlogo;
+	public GameObject SampleArmDrag;
+	public GameObject chardesign;
+	public GameObject fullbodyavatar;
 
 	private void Start(){
 		text = GameObject.Find ("Text (1)");
 		title = GameObject.Find ("REBINDTitle");
 		charColors = GameObject.Find ("CharColors");
 		sampleTorsoMove = GameObject.Find ("SampleTorsoMove");
+		rebindlogo = GameObject.Find ("REBINDLOGO");
+		chardesign = GameObject.Find ("chardesign");
+		fullbodyavatar = GameObject.Find ("fullbodyavatar");
 	}
 
 	private void Update()
@@ -28,6 +35,9 @@ public class Credits : MonoBehaviour
 		title.transform.Translate (Vector3.up * Time.deltaTime * speed);
 		charColors.transform.Translate (Vector3.up * Time.deltaTime * speed);
 		sampleTorsoMove.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		rebindlogo.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		chardesign.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		fullbodyavatar.transform.Translate (Vector3.up * Time.deltaTime * speed);
 		StartCoroutine ("waitFor");
 	}
 
